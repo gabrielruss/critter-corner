373d422d6613af49b803b85f0b4d82c29e8b8dd1
Gabriel Russ <gabrielruss88@gmail.com>
2023-09-14 15:08:10 -0400

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,33 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Critter Corner`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "EhFZnhz8O6BqvxtlhEWaL7Dty2gSkLTC8bvzQn6h_nI",
+      "spaceId": "en71uqo9u0v4"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
+
+export default config;
